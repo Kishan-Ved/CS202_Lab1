fd5b5ac3bcafe799fe27e39b7a8368d489c6c471
Kishan-Ved <kishan.ved@iitgn.ac.in>
2025-01-09 09:08:25 +0530

    some changes

---
+++ a.py
@@ -0,0 +1,35 @@
+a = 2
+b = 3
+
+c = a+b
+
+# Now, we check the value of c
+
+if c==5:
+    print("c is 5")
+else:
+    print("c is not 5")
+
+while c>0:
+    print(c)
+    c = c-1
+
+if c == 0:
+    print("c is 0") 
+
+for i in range(5):
+    print(i)
+
+def f(a,b):
+    return a*b
+
+d = f(a,b)
+
+print(d)
+
+if d == 6:
+    print("d is 6")
+else:
+    print("d is not 6")
+
+print("End of .py file")
