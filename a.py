da72a5f278b8aecc9b419f8723035194c5d06b67
Kishan-Ved <kishan.ved@iitgn.ac.in>
2025-01-09 09:18:57 +0530

    pylint changes

--- a.py
+++ a.py
@@ -1,35 +1,35 @@
-a = 2
-b = 3
+A = 2
+B = 3
 
-c = a+b
+C = A+B
 
-# Now, we check the value of c
+# Now, we check the value of C
 
 # Some changes
 
-if c==5:
-    print("c is 5")
+if C==5:
+    print("C is 5")
 else:
-    print("c is not 5")
+    print("C is not 5")
 
-while c>0:
-    print(c)
-    c = c-1
+while C>0:
+    print(C)
+    C = C-1
 
-if c == 0:
-    print("c is 0") 
+if C == 0:
+    print("C is 0")
 
 for i in range(5):
     print(i)
 
-def f(a,b):
-    return a*b
+def f(A,B):
+    return A*B
 
-d = f(a,b)
+D = f(A,B)
 
-print(d)
+print(D)
 
-if d == 6:
+if D == 6:
     print("d is 6")
 else:
     print("d is not 6")
